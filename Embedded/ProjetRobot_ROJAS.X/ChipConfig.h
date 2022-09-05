00556cd68c175a42e8a4d31cbd85904ec00d9b77
TABLE2 <78029790+Kergourlay-T@users.noreply.github.com>
2022-09-05 09:39:21 +0200

    ro
    oubliez pas de commit la prochaine fois

---
+++ Embedded/ProjetRobot_ROJAS.X/ChipConfig.h
@@ -0,0 +1,15 @@
+#ifndef CHIPCONFIG_H
+
+#define	CHIPCONFIG_H
+
+
+
+#define FCY 40000000
+
+
+
+void InitOscillator();
+
+
+
+#endif	/* CHIPCONFIG_H */
---
+++ Embedded/ProjetRobot_ROJAS.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ProjetRobot_ROJAS.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ProjetRobot_ROJAS.X.production.hex
